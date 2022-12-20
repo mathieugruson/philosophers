1c3f8a67909e081cb1821d847f84fdd3a6927088
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-12-20 19:21:27 +0100

    test

--- philo.c
+++ philo.c
@@ -6,26 +6,64 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:40:07 by mgruson           #+#    #+#             */
-/*   Updated: 2022/12/20 16:03:51 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/12/20 19:18:13 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void*	routine(void)
-{
-    printf("test\n");
+pthread_mutex_t mutexFuel;
+pthread_cond_t condFuel;
+int fuel = 0;
+
+void* fuel_filling(void* arg) {
+    for (int i = 0; i < 5; i++) {
+        pthread_mutex_lock(&mutexFuel);
+        fuel += 15;
+        printf("Filled fuel... %d\n", fuel);
+        pthread_mutex_unlock(&mutexFuel);
+        pthread_cond_signal(&condFuel);
+        sleep(1);
+    }
 }
 
-int main(int argc, char *argv[])
-{ 
-    (void)argc;
-    (void)argv; 
-    pthread_t t1;
-    
-	if (pthread_create(&t1, NULL, (void *)routine, NULL) != 0)
-		return (1);
-    if (pthread_join(t1, NULL))
-        return (1);
-    return (0);    
+void* car(void* arg) {
+    pthread_mutex_lock(&mutexFuel);
+    while (fuel < 40) {
+        printf("No fuel. Waiting...\n");
+        pthread_cond_wait(&condFuel, &mutexFuel);
+        // Equivalent to:
+        // pthread_mutex_unlock(&mutexFuel);
+        // wait for signal on condFuel
+        // pthread_mutex_lock(&mutexFuel);
+    }
+    fuel -= 40;
+    printf("Got fuel. Now left: %d\n", fuel);
+    pthread_mutex_unlock(&mutexFuel);
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t th[2];
+    pthread_mutex_init(&mutexFuel, NULL);
+    pthread_cond_init(&condFuel, NULL);
+    for (int i = 0; i < 2; i++) {
+        if (i == 1) {
+            if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0) {
+                perror("Failed to create thread");
+            }
+        } else {
+            if (pthread_create(&th[i], NULL, &car, NULL) != 0) {
+                perror("Failed to create thread");
+            }
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
+    pthread_mutex_destroy(&mutexFuel);
+    pthread_cond_destroy(&condFuel);
+    return 0;
 }
--- philo.h
+++ philo.h
@@ -6,16 +6,17 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:42:40 by mgruson           #+#    #+#             */
-/*   Updated: 2022/12/20 12:44:06 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/12/20 19:18:17 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H 
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 #endif
